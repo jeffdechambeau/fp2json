666cab37760ed9f80f47572be7e86ddfd8dbbef6
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-11-03 08:55:58 -0400

    add dir

--- annotator/build.js
+++ annotator/build.js
@@ -1,20 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-// Directory where your JS files are located
 const jsDirectory = './src';
 
-// Path to your HTML template
 const htmlTemplatePath = './src/template.html';
 const htmlOutputPath = './dist/index.html';
 
-// Placeholder in your HTML where scripts will be injected
 const injectionPlaceholder = '<!-- INJECT_SCRIPTS -->';
 
-// Read the HTML template
 let htmlContent = fs.readFileSync(htmlTemplatePath, 'utf8');
 
-// Read all JS files from the directory and create script tags
+if (!fs.existsSync(htmlOutputPath)) {
+  fs.mkdirSync(htmlOutputPath, { recursive: true });
+}
+
 fs.readdir(jsDirectory, (err, files) => {
   if (err) {
     console.error('Error reading JS directory:', err);
@@ -27,10 +26,8 @@ fs.readdir(jsDirectory, (err, files) => {
     .join('\n')}</script>`;
   console.log({ scriptTags });
 
-  // Replace the placeholder with the actual script tags
   htmlContent = htmlContent.replace(injectionPlaceholder, scriptTags);
 
-  // Write the updated HTML content to the output file
   fs.writeFileSync(htmlOutputPath, htmlContent, 'utf8');
   console.log('Scripts injected and HTML file created:', htmlOutputPath);
 });
